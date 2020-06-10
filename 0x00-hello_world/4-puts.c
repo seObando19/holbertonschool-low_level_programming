b06a3f8a635687fa422a33a08414650327be6da2
seObando19 <sebastiano-10@hotmail.com>
2020-06-10 23:17:29 +0000

    Update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #define size 51
+/*
+ * Description: Using puts
+*/
 int main(void)
 {
-	char cadena[size] = "Programming is like building a multilingual puzzle";
-	puts(cadena);
-	return 0;
+char cadena[size] = "Programming is like building a multilingual puzzle";
+puts(cadena);
+return (0);
 }
