dd267ad1d9804e0a2c2b98998f4c58ecb262ed0a
seObando19 <sebastiano-10@hotmail.com>
2020-06-11 22:17:59 +0000

    Completed task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - print numbers
+  *
+  *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
