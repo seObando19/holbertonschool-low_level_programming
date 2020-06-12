d1a61911ac293bb87719f538b191a4e89ad5a1e4
seObando19 <sebastiano-10@hotmail.com>
2020-06-12 02:10:01 +0000

    Completed task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - print_tebahpla
+  *
+  *Return: Always 0 (Succcess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
