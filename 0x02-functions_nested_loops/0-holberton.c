8f717c392bf3a20e2d3f84da03f998f0ec4ebdc6
seObando19 <sebastiano-10@hotmail.com>
2020-06-15 20:50:07 +0000

    Updated task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	unsigned int i;
-	char nom[9] = {'H','o','l','b','e','r','t','o','n'};
+	char nom[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 
 	for (i = 0; i < sizeof(nom); i++)
 	{
