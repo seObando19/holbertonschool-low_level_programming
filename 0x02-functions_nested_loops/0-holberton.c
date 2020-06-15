451a6962931e6e20e6ae5810f7cfba979e173ada
seObando19 <sebastiano-10@hotmail.com>
2020-06-15 20:01:02 +0000

    Completed task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  *main - Print 'Holberton'
+  *
+  *Return: void
+*/
+int main(void)
+{
+	unsigned int i;
+	char nom[9] = {'H','o','l','b','e','r','t','o','n'};
+
+	for (i = 0; i < sizeof(nom); i++)
+	{
+		_putchar(nom[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
