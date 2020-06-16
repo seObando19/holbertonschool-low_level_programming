c8b5bc0cd7babad4d5b4c931badca0d7b9ad2703
seObando19 <sebastiano-10@hotmail.com>
2020-06-16 21:44:54 +0000

    Completed task 10 and add test 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  *add - make a addition
+  *@a: variable1
+  *@b: variable2
+  *Return: value of addition
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
