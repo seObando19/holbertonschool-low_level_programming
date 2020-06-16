729635c46782d92cea5fbf7c6d414469766e1044
seObando19 <sebastiano-10@hotmail.com>
2020-06-16 01:25:54 +0000

    Completed task 5 and add test 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+  *print_sign - print option switch value of n
+  *@n:the character to check
+  *Return: 1- n > 0 | 0- n == 0 | -1 n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
