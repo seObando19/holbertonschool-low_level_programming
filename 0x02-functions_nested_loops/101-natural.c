d2080421ce6fd8e868a62347261dc112829c635f
seObando19 <sebastiano-10@hotmail.com>
2020-06-17 22:55:14 +0000

    Update task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
+  *main - Function for print addition of n
   *
-  *
-  *
+  *Return: Don´t return value
  */
 int main(void)
 {
 	int n;
 
-	while(n < 1024)
+	n = 3;
+	while (n < 1024)
 	{
 		n = n + 3;
 	}
-	printf("%d\n",n);
+	printf("%d\n", n);
 	return (0);
 }
