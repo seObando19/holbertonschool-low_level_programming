47e2d2a5d2ee9abe788e4b76d0f9343c1bbc383f
seObando19 <sebastiano-10@hotmail.com>
2020-06-18 20:31:44 +0000

    Completed task 7 and add test 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+  *print_diagonal - print the diagonal
+  *@n:value to check
+  *Return: don´t return
+ */
+void print_diagonal(int n)
+{
+	int i = 0, cont = 0;
+
+	if (!n <= 0)
+	{
+		while (i < n)
+		{
+			while (cont < i)
+			{
+				_putchar(32);
+				cont++;
+			}
+			cont = 0;
+			_putchar(92);
+			_putchar('\n');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
