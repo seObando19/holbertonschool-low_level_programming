4c0342d7ff15dcec2ea8ed8b8aaa1ae005fe42d8
seObando19 <sebastiano-10@hotmail.com>
2020-06-18 16:58:25 +0000

    Completed task 4 and add test 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+  *print_numbers - Print numbers 0-9 do not print 2 and	4
+  *
+  *Return: always (0)
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		if (!(i == 50) && !(i == 52))
+			_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
