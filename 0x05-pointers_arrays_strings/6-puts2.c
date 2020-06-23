e12b6d7c1f4eafb05a4ddb2aaa337a6ed0f49fbb
seObando19 <sebastiano-10@hotmail.com>
2020-06-23 17:22:23 +0000

    Completed task 6 and add test 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  *puts2 - print the every other characters
+  *@str: char to check
+  *Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0, a;
+
+	while (str[i] != '\0')
+		i++;
+	i--;
+	for (a = 0; a < i; a++)
+	{
+		if (str[a] % 2 == 0)
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+}
