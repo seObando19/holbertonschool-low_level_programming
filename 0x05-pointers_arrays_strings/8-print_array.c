2805be134a1596408179bfe20606c525db4695b8
seObando19 <sebastiano-10@hotmail.com>
2020-06-24 00:25:33 +0000

    Completed task 8 and add test 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  *print_array -print array
+  *@a:pointer of the array
+  *@n:numbers of the array
+  *Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", a[i]);
+
+		if ((i + 1) != 5)
+		{
+			printf("%c", 44);
+			printf("%c", 32);
+		}
+	}
+	printf("\n");
+}
