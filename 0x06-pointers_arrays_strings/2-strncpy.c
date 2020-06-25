c4d8db3e2913cc9fa55f7e65b8bd7b1a2b920af9
seObando19 <sebastiano-10@hotmail.com>
2020-06-25 19:13:21 +0000

    Completed task 2 and add test 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  * *_strncpy - copies a string.
+  *@dest: pointer 1 to check
+  *@src: pointer 2 to check
+  *@n: size bytes
+  *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+			dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
