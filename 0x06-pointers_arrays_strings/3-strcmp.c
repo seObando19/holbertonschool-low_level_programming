e2fd2fec7a0364f5fabdf8b46eb7c14f396880e0
seObando19 <sebastiano-10@hotmail.com>
2020-06-25 20:38:02 +0000

    Updated task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,22 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0, b = 0;
+	int a = 0;
 
-	while (s1[a] != '\0')
+	while (s1[a] == s2[a])
+	{
+		if (s1[a] == '\0')
+			return (0);
 		a++;
-	while (s2[b] != '\0')
-		b++;
-	if (a == b)
-	{
-		return (0);
 	}
-	else if (a > b)
-	{
-		return (15);
-	}
-	else
-	{
-		return (15 * -1);
-	}
+	return (s1[a] - s2[a]);
 }
