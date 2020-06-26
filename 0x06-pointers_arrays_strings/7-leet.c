01a9841eede6d7dd0e07383a6094f8063e111bf0
seObando19 <sebastiano-10@hotmail.com>
2020-06-26 18:43:00 +0000

    Updated v2 task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,16 +11,19 @@ char *leet(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = 52;
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = 51;
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = 48;
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = 55;
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = 49;
+		if (s[i] != '\0')
+		{
+			if (s[i] == 'a' || s[i] == 'A')
+				s[i] = 52;
+			else if (s[i] == 'e' || s[i] == 'E')
+				s[i] = 51;
+			else if (s[i] == 'o' || s[i] == 'O')
+				s[i] = 48;
+			else if (s[i] == 't' || s[i] == 'T')
+				s[i] = 55;
+			else if (s[i] == 'l' || s[i] == 'L')
+				s[i] = 49;
+		}
 		i++;
 	}
 	return (s);
