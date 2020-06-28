20af17443e881951daf891622a0b35dcbed52b4f
seObando19 <sebastiano-10@hotmail.com>
2020-06-28 19:19:10 +0000

    Completed task 2 amd add test 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+  * *_strchr - locates a character in a string.
+  *@s: pointer
+  *@c:letter to find
+  *Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+	
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	else
+	{
+		return ('\0');
+	}
+}
