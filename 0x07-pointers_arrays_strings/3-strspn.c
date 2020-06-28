cb5bd29a5f16a734ba20074c2395a761af8ebad0
seObando19 <sebastiano-10@hotmail.com>
2020-06-28 22:46:56 +0000

    Updated task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,12 +8,13 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, a;
+	unsigned int i = 0;
+	int a;
 	unsigned int cont = 0;
 
 	for (i = 0; accept[i] != '\0'; i++)
 	{
-		for (a = 0; s[a] != ' '; a++)
+		for (a = 0; s[a] > ' '; a++)
 		{
 			if (accept[i] == s[a])
 				cont++;
