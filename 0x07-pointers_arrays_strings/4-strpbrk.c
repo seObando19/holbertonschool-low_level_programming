f0b5ae34a5f93b98e629dfc7da0bb995a907f767
seObando19 <sebastiano-10@hotmail.com>
2020-06-29 00:17:10 +0000

    Updated v4 task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,23 +8,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-	int a;
+	unsigned int i;
+	unsigned int a = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] > '\0'; i++)
 	{
 		for (a = 0; accept[a] != '\0'; a++)
 		{
 			if (s[i] == accept[a])
-				return (&s[i]);
+				return (s + i);
 		}
 	}
-	if (s[i] == accept[a])
-	{
-		return (&s[i]);
-	}
-	else
-	{
-		return ('\0');
-	}
+	return (s + i);
 }
