bec4a700120916614ab72ef754cc95d70426d6fd
seObando19 <sebastiano-10@hotmail.com>
2020-07-01 19:19:13 +0000

    Completed task 1 and add test 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  *_print_rev_recursion - prints a string in reverse.
+  *@s: pointer the string
+  *Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		return;
+	}
+}
