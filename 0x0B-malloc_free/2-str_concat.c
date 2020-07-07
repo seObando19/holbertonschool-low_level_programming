412bcc35b24957b528a3240ff95f960ce5d0770d
seObando19 <sebastiano-10@hotmail.com>
2020-07-07 22:49:00 +0000

    Completed task 2 and add test 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * _stlen - find the length of string
+  *@a: pointer to exam
+  *Return: length of string
+ */
+int _stlen(char *a)
+{
+	int l = 0;
+
+	while (a[l] != '\0')
+	{
+		l++;
+	}
+	return (l);
+}
+/**
+  * *str_concat - concatenates two strings.
+  *@s1: pointer of first string
+  *@s2: pointer of second string
+  *Return: Null if proccess failed or pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, x, len1 = 0, len2 = 0;
+	char *ar;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	len1 = _stlen(s1);
+	len2 = _stlen(s2);
+	ar = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s1 || !s2)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		ar[i] = s1[i];
+	}
+	for (x = 0; x < len2; x++)
+	{
+		ar[i] = s2[x];
+		i++;
+	}
+	ar[i] = '\0';
+	return (ar);
+}
