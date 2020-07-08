1a284408b6872a053de76ec6d0d10377f3a6f2b3
seObando19 <sebastiano-10@hotmail.com>
2020-07-08 14:22:26 +0000

    Completed task 3 and add test 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * **alloc_grid - pointer to a 2 dimensional array of int.
+  *@width: width of array
+  *@height: height of array
+  *Return: double pointer of array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, a, **ai;
+
+	if (width <= 0 || height <= 0)
+	{
+		return ('\0');
+	}
+	ai = (int **)malloc(height * sizeof(int *));
+	for (i = 0; i < height; i++)
+	{
+		ai[i] = malloc(width * sizeof(int));
+	}
+	if (!ai)
+	{
+		return ('\0');
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (a = 0; a < width; a++)
+		{
+			ai[i][a] = 0;
+		}
+	}
+	return (ai);
+}
