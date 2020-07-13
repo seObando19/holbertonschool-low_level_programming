24f36c512abffe8446f7218c36067d655981c5c6
seObando19 <sebastiano-10@hotmail.com>
2020-07-13 22:11:15 +0000

    Completed task 0 and add test 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+  *struct dog - Define a new type
+  *@name: First member pointer
+  *@age: Second member
+  *@owner: Third member pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* _DOG_H_ */
