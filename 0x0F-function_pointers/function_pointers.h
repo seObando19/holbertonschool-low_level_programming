09b661dff74f6b65c781de658a263cdf8977b20e
seObando19 <sebastiano-10@hotmail.com>
2020-07-15 20:11:40 +0000

    Updated file for task 0 and 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-/* void array_iterator(int *array, size_t size, void (*action)(int)); */
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* _FUNCTION_POINTERS_H_ */
