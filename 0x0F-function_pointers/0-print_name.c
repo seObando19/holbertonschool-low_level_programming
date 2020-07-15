7308ee55d150a225a3dc6554a02fbaf418be8c55
seObando19 <sebastiano-10@hotmail.com>
2020-07-15 20:21:47 +0000

    Updated v6 task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
   *print_name - print name
@@ -13,4 +13,5 @@ void print_name(char *name, void (*f)(char *))
 	{
 		f(name);
 	}
+	return;
 }
