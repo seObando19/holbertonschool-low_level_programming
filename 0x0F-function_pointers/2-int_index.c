26f0ae92fc00c3c697864ac944e1b0623c9aa97c
seObando19 <sebastiano-10@hotmail.com>
2020-07-15 21:50:10 +0000

    Completed task 2 and add test 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+  *int_index - searches for an integer.
+  *@array: pointer of array
+  *@size: size of array
+  *@cmp: pointer of functions
+  *Return: -1 if faile process else indice
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, res = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		res = cmp(array[i]);
+		if (res != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
