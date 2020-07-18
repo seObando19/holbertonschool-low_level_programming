ae4f12cf5f0999ab816a0f0008288c30fafb69f5
seObando19 <sebastiano-10@hotmail.com>
2020-07-18 17:00:14 +0000

    Completed task 0 and add test 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+  *
+  *
+  *
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
+}
