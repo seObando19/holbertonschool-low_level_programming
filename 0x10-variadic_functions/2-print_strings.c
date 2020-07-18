83952ff8c5033f56c45bdc0b1c1e34ffaeabf81b
seObando19 <sebastiano-10@hotmail.com>
2020-07-18 17:19:01 +0000

    Updated task 2 added documentation

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,10 +2,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
-  *
-  *
-  *
-  *
+  *print_strings - prints strings
+  *@separator: string what separator of numbers
+  *@n: strings of parameters passed of function
+  *Return: void
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -17,10 +17,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		ptr = va_arg(list, char*);
-		while (ptr[a] != '\0')
+		if (ptr != NULL)
 		{
-			printf("%c", ptr[a]);
-			a++;
+			while (ptr[a] != '\0')
+			{
+				printf("%c", ptr[a]);
+				a++;
+			}
+		}
+		else
+		{
+			printf("(nil)");
 		}
 		if ((separator != NULL) && (i + 1) != n)
 		{
