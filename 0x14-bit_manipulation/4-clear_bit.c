6edde3de6137ff75739d9acbd8f4c6b694ed52bb
seObando19 <sebastiano-10@hotmail.com>
2020-07-31 19:50:30 +0000

    Completed task 4 and add test 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  *clear_bit -  sets the value of a bit to 0
+  * @n: pointer to the number
+  * @index: index
+  * Return: 1 if it worked, or -1 if an error occurred
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int l = 1;
+
+	l <<= index;
+	if (index > sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n = *n & ~(l);
+	return (1);
+}
