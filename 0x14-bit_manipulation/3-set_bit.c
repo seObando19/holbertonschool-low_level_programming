1025f92b4e6f935bd78dcf970ba9e4da351ede2c
seObando19 <sebastiano-10@hotmail.com>
2020-07-31 17:56:12 +0000

    Completed task 3 and add test 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  *set_bit - sets the value of a bit
+  *to 1 at a given index.
+  *@n: pointer the value
+  *@index: position to change
+  *Return: 1 if proccess success -1 faile
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (8 * sizeof(n) - 1))
+	{
+		return (-1);
+	}
+	*n = *n | (1 << index);
+	return (1);
+}
