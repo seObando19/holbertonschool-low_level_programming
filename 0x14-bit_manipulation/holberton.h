b25449e2881af6d1f62d9c49ee1113384907d7ea
seObando19 <sebastiano-10@hotmail.com>
2020-07-31 14:12:45 +0000

    Updated file for task 1

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,5 +2,7 @@
 #define _HOLBERTON_H_
 #include <stdio.h>
 #include <stdlib.h>
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif /* _HOLBERTON_H_ */
