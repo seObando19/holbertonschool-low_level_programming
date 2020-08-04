00e608694abc710ca3ad386b91627590a9c8a842
seObando19 <sebastiano-10@hotmail.com>
2020-08-04 22:13:05 +0000

    Updated file for task 0

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,16 +1,9 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-/**
-  *header for open
-*/
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-/**
-  *header for write, read and close
-*/
 #include <unistd.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
