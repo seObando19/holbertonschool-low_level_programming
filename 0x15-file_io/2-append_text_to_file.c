0701f96b2a0207e129e7d987eca9bbab63920c96
seObando19 <sebastiano-10@hotmail.com>
2020-08-05 17:10:08 +0000

    Updated task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		i++;
 	len = write(file, text_content, i);
 	close(file);
-	if (!len)
+	if (len == -1)
 		return (-1);
 	return (1);
 }
