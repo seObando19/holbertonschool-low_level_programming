0015f2f41d753bd024cced19b0e27f4d9e8fe38a
seObando19 <sebastiano-10@hotmail.com>
2020-08-06 23:10:40 +0000

    Updated file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -4,6 +4,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
