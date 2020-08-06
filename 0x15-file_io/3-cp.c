238b9aba3f484d9bc907bdc4278762266b2b0300
seObando19 <sebastiano-10@hotmail.com>
2020-08-06 23:11:22 +0000

    Updated v3 task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,10 @@ int main(int argc, char *argv[])
 	char buf[BUFSIZ];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r < 0)
 	{
@@ -25,7 +28,8 @@ int main(int argc, char *argv[])
 		if (fd_w < 0 || write(fd_w, buf, rd) != rd)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_r), exit(99);
+			close(fd_r);
+			exit(99);
 		}
 	}
 	if (rd < 0)
@@ -33,7 +37,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_r = close(fd_r), fd_w = close(fd_w);
+	fd_r = close(fd_r);
+	fd_w = close(fd_w);
 	if (fd_r < 0 || fd_w < 0)
 	{
 		if (fd_r < 0)
