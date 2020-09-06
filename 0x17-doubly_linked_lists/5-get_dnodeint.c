f6d62b0ebf78da5d27d004e84d0ff4226df91ea7
seObando19 <sebastiano-10@hotmail.com>
2020-09-06 08:13:23 +0000

    Updated v3 for task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *findNode;
-	unsigned int position;
+	unsigned int position = 0;
 
 	if (!(head == NULL) || !(index == 0))
 	{
