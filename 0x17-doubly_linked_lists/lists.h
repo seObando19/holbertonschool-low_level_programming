017efa1c436e60321737658e1884b0978bc960a6
seObando19 <sebastiano-10@hotmail.com>
2020-09-06 10:07:07 +0000

    Updated file for task 8

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,4 +29,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 size_t lenList(const dlistint_t *h);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 #endif /* _LISTS_H_ */
