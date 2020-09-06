b89f6eddfd5028cdd45db89df54be5f10f74de0f
seObando19 <sebastiano-10@hotmail.com>
2020-09-06 06:56:21 +0000

    Updated file v2 in task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,3 +31,28 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	return (newnode);
 }
+
+/**
+ *add_dnodeint - adds a new node at the
+ *beginning of a dlistint_t list.
+ *@head: Double pointer of the list
+ *@n: value of the list
+ *Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+
+	newnode = malloc(sizeof(dlistint_t));
+	if (head == NULL)
+		return (NULL);
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = *head;
+	newnode->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = newnode;
+	*head = newnode;
+	return (newnode);
+}
