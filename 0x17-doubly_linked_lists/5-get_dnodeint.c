1d51a85d01ad6d8113c283947d430e9fd399544e
seObando19 <sebastiano-10@hotmail.com>
2020-09-06 08:11:59 +0000

    Updated task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (!(head == NULL) || !(index == 0))
 	{
 		findNode = head;
-		while (head)
+		while (head != NULL)
 		{
 			if (position == index)
 			{
